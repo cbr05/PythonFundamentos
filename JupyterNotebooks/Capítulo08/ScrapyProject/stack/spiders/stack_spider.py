780daf19c7063edfef1fb4f045f0b104c91cfd9a
Daniel Mendes <danielm@dsacademy.com.br>
2016-08-25 20:50:05 -0700

    Update Agosto 2016

---
+++ JupyterNotebooks/Capítulo08/ScrapyProject/stack/spiders/stack_spider.py
@@ -0,0 +1,22 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+
+from stack.items import StackItem
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StackItem()
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# PythonFundamentos
-Repositório do Curso Online Python Fundamentos
+# Python Fundamentos
+
+Data Science Academy - Python Fundamentos para Análise de Dados
+
+Seja Bem-vindo ao Repositório do curso Python Fundamentos para Análise de Dados. Aqui você encontra todos os Jupyter Notebooks usados no curso, bem como os exercícios.
+
+http://www.datascienceacademy.com.br
+
+
+
