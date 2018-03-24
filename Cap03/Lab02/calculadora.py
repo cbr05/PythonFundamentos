f33f9cb8d899570972494bb3d427484483822bd1
Fernanda Magalhães <fernandam@dsacademy.com.br>
2018-03-23 18:51:17 -0700

    Update Cap03

---
+++ Cap03/Lab02/calculadora.py
@@ -0,0 +1,6 @@
+# Calculadora em Python
+
+# Desenvolva uma calculadora em Python com tudo que voc6e aprendeu nos capítulos 2 e 3. A solução será apresentada no próximo capítulo!
+
+print("\n******************* Python Calculator *******************")
+
